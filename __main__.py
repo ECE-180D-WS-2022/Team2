0d55be114daf2f0c68038028359f7b14278dcd1b
dlin7195 <70040558+dlin7195@users.noreply.github.com>
2022-03-13 10:50:48 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -40,37 +40,43 @@ try:
     with m as source: r.adjust_for_ambient_noise(source,duration=5)
     print("Set minimum energy threshold to {}".format(r.energy_threshold))
     while True:
-        print("Say something!")
-        with m as source: audio = r.listen(source)
-         
-        print("Got it! Now to recognize it...")
-        try:
-            # recognize speech using Google Speech Recognition
-            value = r.recognize_google(audio)
-            value=str(value).split(" ",)
-            value=value[0].lower()
-            if str(value) in heallist:
-                value='heal'
-                flag = 1
-            elif str(value) in dlist:
-                value='defend'
-                flag = 2
-            else:
-                print(str(value))
-                print('try again, that word is not part of the game')
-                flag = 0
-             
-            client.publish('ECE180D/team2', flag, qos=1)
+        # keyboard input to trigger listening (temp)
+        inp = input("\nType 'Start' to begin listening:\n")
+        inp = inp.lower()
+        if (inp == "start"):
+            print("Say something!")
+            with m as source: audio = r.listen(source)
+            print("Got it! Now to recognize it...")
+            try:
+                # recognize speech using Google Speech Recognition
+                value = r.recognize_google(audio)
+                value=str(value).split(" ",)
+                value=value[0].lower()
+                if str(value) in heallist:
+                    value='heal'
+                    flag = 1
+                elif str(value) in dlist:
+                    value='defend'
+                    flag = 2
+                else:
+                    print(str(value))
+                    print('try again, that word is not part of the game')
+                    flag = 0
+                client.publish('ECE180D/team2', flag, qos=1)
             
             # we need some special handling here to correctly print unicode characters to standard output
-        #    if str is bytes:  # this version of Python uses bytes for strings (Python 2)
-         #       print("You said {}".format(value).encode("utf-8"))
-         #   else:  # this version of Python uses unicode for strings (Python 3+)
+            #    if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+            #       print("You said {}".format(value).encode("utf-8"))
+            #   else:  # this version of Python uses unicode for strings (Python 3+)
             print("You said {}".format(value))
-        except sr.UnknownValueError:
-            print("Oops! Didn't catch that")
-        except sr.RequestError as e:
-            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+            except sr.UnknownValueError:
+                print("Oops! Didn't catch that")
+            except sr.RequestError as e:
+                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        else:
+            print("Unrecognized input, try again")
+            continue
+            
 except KeyboardInterrupt:
     # disconnect
     client.loop_stop()
